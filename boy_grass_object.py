72f18d3a960b948d52464b88ed2eeb66bb629308
lingk4231075 <strbryjam1220@gmail.com>
2024-10-08 10:29:45 +0900

    잔디 클래스 정의 및 잔디 그리기

--- boy_grass_object.py
+++ boy_grass_object.py
@@ -1,6 +1,13 @@
 from pico2d import *
 
 # Game object class here
+class Grass:
+    def __init__(self):
+        self.image = load_image('grass.png')
+    def update(self):
+        pass
+    def draw(self):
+        self.image.draw(400, 30)
 
 def handle_events():
     global running
@@ -13,13 +20,16 @@ def handle_events():
 
 def reset_world():
     global running
+    global grass
     running = True
-
+    grass = Grass()
 def update_world():
+    grass.update() #객체 상태 시뮬레이션
     pass
 
 def render_world():
     clear_canvas()
+    grass.draw()
     update_canvas()
     pass
 running = True
