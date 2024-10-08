5e87acfc5c1217ff451136f2e9273b06fcf9d78f
lingk4231075 <strbryjam1220@gmail.com>
2024-10-08 11:09:15 +0900

    Drill 7 제출 (2021182035 조재민)
    Drill #7. 공들의 자유 낙하 구현

--- boy_grass_object.py
+++ boy_grass_object.py
@@ -24,6 +24,27 @@ class Boy:
         self.image.clip_draw(self.frame * 100, 0, 100, 100, self.x, self.y)
 
 
+class Ball:
+    def __init__(self):
+        self.x, self.y = randint(50, 750), 599
+        self.ballsize = randint(0, 1)
+        self.speed = randint(20, 100) / 10
+        if self.ballsize == 0:
+            self.ballsize = 21
+            self.image = load_image('ball21x21.png')
+        else:
+            self.ballsize =41
+            self.image = load_image('ball41x41.png')
+    def update(self):
+        self.y -= self.speed
+        if self.y - (self.ballsize / 2) > 30 + 20:
+            pass
+        else:
+            self.y = 30 + 20 + (self.ballsize / 2)
+    def draw(self):
+        self.image.clip_draw(0, 0, self.ballsize, self.ballsize, self.x, self.y)
+
+
 def handle_events():
     global running
     events = get_events()
@@ -37,21 +58,31 @@ def reset_world():
     global running
     global grass
     global team
+    global world
 
     running = True
+    world = []
+
     grass = Grass()
+    world.append(grass)
+
     team = [Boy() for i in range(11)]
+    world += team
+
+    balls = [Ball() for i in range(20)]
+    world += balls
+
 def update_world():
     grass.update() #객체 상태 시뮬레이션
-    for boy in team:
-        boy.update()
+    for o in world:
+        o.update()
     pass
 
 def render_world():
     clear_canvas()
-    grass.draw()
-    for boy in team:
-        boy.draw()
+    for o in world:
+        o.draw()
+
     update_canvas()
     pass
 running = True
